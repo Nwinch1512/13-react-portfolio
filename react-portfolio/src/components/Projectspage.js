f526016b70c5b208021c26c54e2ff4317b4de34c
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-09 13:56:11 +0000

    Project template populating with json data and rendering on port 3000

--- react-portfolio/src/components/Projectspage.js
+++ react-portfolio/src/components/Projectspage.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "../styles/Projectspage.css";
 import Project from "./Project";
+import Wrapper from "./Wrapper";
+import projects from "../projects.json";
 
-function Projectspage() {
-  return <section></section>;
+class Projectspage extends Component {
+  state = { projects };
+
+  render() {
+    return (
+      <Wrapper>
+        {this.state.projects.map((project) => (
+          <Project
+            title={project.title}
+            image={project.image}
+            url={project.url}
+            GitHub={project.GitHub}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default Projectspage;
