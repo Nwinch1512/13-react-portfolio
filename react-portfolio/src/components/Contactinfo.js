94f8966bef193a6ca556f7012dd5710e257e4e95
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-10 15:59:58 +0000

    Added styling to contact page

--- react-portfolio/src/components/Contactinfo.js
+++ react-portfolio/src/components/Contactinfo.js
@@ -3,44 +3,48 @@ import "../styles/Contactspage.css";
 
 function Contact() {
   return (
-    <section classNameName="page-section contact" id="contact">
-      <div classNameName="contact">
-        <h2>Contact</h2>
-        <p>
-          Please read my <a href="../CV.pdf">CV</a> for information on my
-          background. Feel free to{" "}
-          <a href="mailto: naomi.winchurch@hotmail.co.uk">email</a> me if you
-          have any questions or fill out the contact form below.
-        </p>
+    <section className="page-section contact">
+      <div className="cardContainer col-lg-12 col-md-12 col-sm-3">
+        <div className="contact">
+          <div className="card-body" id="contact">
+            <h2 className="card-title">Contact </h2>
+            <div className="contact-info">
+              <p className="contact">
+                Please read my <a href="../CV.pdf">CV</a> for information on my
+                background. Feel free to{" "}
+                <a href="mailto: naomi.winchurch@hotmail.co.uk">email</a> me if
+                you have any questions or fill out the contact form below.
+              </p>
+              <p className="contact">
+                Links to my GitHub and Linkedin profiles have also been included
+                below for reference.
+              </p>
+              <p className="contact">
+                Here is a link to my{" "}
+                <a
+                  classNameName="btn"
+                  href="https://github.com/Nwinch1512"
+                  role="button"
+                >
+                  Github
+                </a>{" "}
+                profile
+              </p>
+              <p className="contact">
+                Please refer to my{" "}
+                <a
+                  classNameName="btn"
+                  href="https://www.linkedin.com/in/naomiwinchurch/"
+                  role="button"
+                >
+                  Linkedin
+                </a>{" "}
+                page for more information on my work experience.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <ul>
-        <p>
-          Links to my GitHub and Linkedin profiles have also been included below
-          for reference.
-        </p>
-        <p>
-          Here is a link to my{" "}
-          <a
-            classNameName="btn"
-            href="https://github.com/Nwinch1512"
-            role="button"
-          >
-            Github
-          </a>{" "}
-          profile
-        </p>
-        <p>
-          Please refer to my{" "}
-          <a
-            classNameName="btn"
-            href="https://www.linkedin.com/in/naomiwinchurch/"
-            role="button"
-          >
-            Linkedin
-          </a>{" "}
-          page for more information on my work experience.
-        </p>
-      </ul>
     </section>
   );
 }
