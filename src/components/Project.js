9749100c7380db862f0ad6a5f016b1a52b88bbb3
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-12 15:18:45 +0000

    Removing unused imports and images

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,22 +3,6 @@ import "../styles/Project.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-const mealPlannerImg = require("../images/01-meal-planner.jpeg");
-const weatherDashboardImg = require("../images/02-weather-dashboard.jpeg");
-const workdaySchedulerImg = require("../images/03-work-day-scheduler.jpeg");
-const javaScriptQuizImg = require("../images/04-javascript-quiz.jpeg");
-const passwordGeneratorImg = require("../images/05-password-generator.jpeg");
-const bootstrapPortfolioImg = require("../images/06-bootstrap-portfolio.jpeg");
-
-const projectImages = [
-  mealPlannerImg,
-  weatherDashboardImg,
-  workdaySchedulerImg,
-  javaScriptQuizImg,
-  passwordGeneratorImg,
-  bootstrapPortfolioImg,
-];
-
 function Project(props) {
   return (
     <div className="card">
