5b044117cb8f359275ebdb4fc2088b8f34d276ef
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-14 12:24:49 +0000

    Fixed mobile view menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import $ from "jquery";
-// import Popper from "popper.js";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery/dist/jquery";
+import "popper.js/dist/popper";
+import "bootstrap/dist/js/bootstrap";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
