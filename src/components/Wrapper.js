d98fa1c9002e0fce84f57528bd1232d3d5cd8721
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-03-13 11:51:44 +0000

    Added background image to projects page and increased icon font size

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "../styles/Wrapper.css";
 
+const marbleImg = require("../images/pexels-henry-&-co-2341290.jpg");
+
 function Wrapper(props) {
   return (
     <div className="cardContainer col-lg-12 col-md-12 col-sm-3">
       <h2 className="project card-title">Projects</h2>
-      <div className="wrapper">{props.children}</div>
+      <div
+        className="wrapper"
+        style={{ backgroundImage: "url(" + marbleImg + ")" }}
+      >
+        {props.children}
+      </div>
     </div>
   );
 }
