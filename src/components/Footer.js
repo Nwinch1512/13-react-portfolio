4632f3ecb04a277a77f88481e77039089394cee6
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-03-15 14:31:30 +0000

    Added styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,57 +1,35 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React from "react";
 import "../styles/Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressBook } from "@fortawesome/free-solid-svg-icons";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer className="footer">
       <div id="contact-methods">
         <a
-          className="btn"
+          className="btn-email"
           href="mailto: naomi.winchurch@hotmail.co.uk"
           role="button"
         >
-          Email
-        </a>
-        <a className="btn" href="https://github.com/Nwinch1512" role="button">
-          Github
+          <FontAwesomeIcon icon={faEnvelope} className="emailBtn" />
         </a>
         <a
-          className="btn"
-          href="https://twitter.com/NaomiWinchurch"
+          className="btn-github"
+          href="https://github.com/Nwinch1512"
           role="button"
         >
-          Twitter
+          <FontAwesomeIcon icon={faGithub} className="githubBtn" />
+        </a>
+        <a
+          className="btn-linkedin"
+          href="https://www.linkedin.com/in/naomiwinchurch/"
+          role="button"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="linkedinBtn" />
         </a>
       </div>
-      {/* <section class="page-section contact" id="contact">
-        <div class="contact">
-          <h2>Contact</h2>
-        </div>
-        <div id="contact-methods">
-          <a
-            class="btn"
-            href="mailto: naomi.winchurch@hotmail.co.uk"
-            role="button"
-          >
-            Email
-          </a>
-          <a class="btn" href="https://github.com/Nwinch1512" role="button">
-            Github
-          </a>
-          <a
-            class="btn"
-            href="https://twitter.com/NaomiWinchurch"
-            role="button"
-          >
-            Twitter
-          </a>
-        </div>
-      </section> */}
     </footer>
   );
 }
